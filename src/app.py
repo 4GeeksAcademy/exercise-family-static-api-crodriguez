0116e538d1b16889cca0682045b70cc038291f06
CRVanware <crodriguez@vanware.io>
2025-01-13 20:30:40 +0000

    inicial

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
@@ -36,6 +36,31 @@ def handle_hello():
     }
 
 
+    return jsonify(response_body), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_member(member_id)
+    response_body = {
+        "hello": "world",
+        "family": members
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    # this is how you can use the Family datastructure by calling its methods
+    
+    member = request.json
+    jackson_family.add_member(member)
+    
+    response_body = {
+            
+        "New_member": "member"  
+    }
+    
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
